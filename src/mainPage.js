8b77e75be0bfa37662d1f6f8bf84efc90c55ffd0
Tatiana <eyesdwn@gmail.com>
2020-02-22 17:24:44 +0200

    stable version

--- src/mainPage.js
+++ src/mainPage.js
@@ -1,72 +1,113 @@
 import * as API from "./services/movies-api";
+import initMoviePage from "./initFilmPage";
 // import Layout from "./Layout";
-const app = {
-  page: 1
-};
-const refs = {
-  moviesList: document.querySelector("#movies-list"),
-  buttonNext: document.querySelector(".next"),
-  buttonPrev: document.querySelector(".prev"),
-  paginationBoxInfo: document.querySelector(".pagination-box-info")
-};
+const initMainPage = () => {
+  document.querySelector(".js-main > .container").innerHTML = `
+    <ul class="movies-list" id="movies-list"></ul>
+    <div class="pagination-box">
+      <button class="pagination-box-button prev">Prev</button>
+      <span class="pagination-box-info"></span>
+      <button class="pagination-box-button next">Next</button>
+    </div>
+    <button class="scrollTopButton"></button>
+    `;
+  const app = {
+    page: 1,
+    query: ""
+  };
+  const refs = {
+    moviesList: document.querySelector("#movies-list"),
+    buttonNext: document.querySelector(".next"),
+    buttonPrev: document.querySelector(".prev"),
+    paginationBoxInfo: document.querySelector(".pagination-box-info"),
+    input: document.querySelector(".js-search-input"),
+    moviesList: document.querySelector(".movies-list")
+  };
 
-const getTrendingMovies = (page = app.page) => {
-  page === 1 ? refs.buttonPrev.classList.add("hiddenButton") : null;
-  API.fetchTrendingMovies(page)
-    .then(data => {
-      const films = data.reduce((acc, item) => {
-        const { id, poster_path, title } = item;
-        return (acc += `
+  const getTrendingMovies = (page = app.page) => {
+    page === 1 ? refs.buttonPrev.classList.add("hiddenButton") : null;
+    API.fetchTrendingMovies(page)
+      .then(data => {
+        const films = data.reduce((acc, item) => {
+          const { id, backdrop_path, title } = item;
+          return (acc += `
           <li data-id="${id}" class="movie">
-            <img class="poster" src="${poster_path}" />
+            <img class="poster" src="${backdrop_path}" />
             <h2 class="movie-title">${title}</h2>
             <div class="inner"></div>
             </li>
           `);
-      }, "");
-      console.log(films);
-      // .insertAdjacentHTML("beforeend", films);
-      document.querySelector("#movies-list").innerHTML = films;
-    })
-    .catch(err => console.log(err));
-  refs.paginationBoxInfo.textContent = page;
-};
-getTrendingMovies();
+        }, "");
 
-const handleNext = () => {
-  app.page = app.page + 1;
-  getTrendingMovies(app.page);
-  refs.buttonPrev.classList.remove("hiddenButton");
-};
-const handlePrev = () => {
-  app.page = app.page - 1;
-  getTrendingMovies(app.page);
-  refs.buttonPrev.classList.remove("hiddenButton");
-  app.page === 1 ? refs.buttonPrev.classList.add("hiddenButton") : null;
-};
+        // .insertAdjacentHTML("beforeend", films);
+        document.querySelector("#movies-list").innerHTML = films;
+      })
+      .catch(err => console.log(err));
+    refs.paginationBoxInfo.textContent = page;
+  };
+  getTrendingMovies();
 
-refs.buttonNext.addEventListener("click", handleNext);
-refs.buttonPrev.addEventListener("click", handlePrev);
+  const handleNext = () => {
+    app.page = app.page + 1;
+    refs.buttonPrev.classList.remove("hiddenButton");
+    if (app.query) {
+      getMoviesByQuery(app.query);
+      return;
+    }
 
-const getMoviesByQuery = query => {
-  API.fetchMovieByQuery(query).then(data => {
-    const films = data.reduce((acc, item) => {
-      const { id, poster_path, title } = item;
-      return (acc += `
+    getTrendingMovies(app.page);
+  };
+  const handlePrev = () => {
+    app.page = app.page - 1;
+    refs.buttonPrev.classList.remove("hiddenButton");
+    app.page === 1 ? refs.buttonPrev.classList.add("hiddenButton") : null;
+    if (app.query) {
+      getMoviesByQuery(app.query);
+      return;
+    }
+    getTrendingMovies(app.page);
+  };
+
+  refs.buttonNext.addEventListener("click", handleNext);
+  refs.buttonPrev.addEventListener("click", handlePrev);
+
+  const getMoviesByQuery = query => {
+    API.fetchMovieByQuery(query, app.page).then(data => {
+      const films = data.reduce((acc, item) => {
+        const { id, backdrop_path, title } = item;
+        return (acc += `
             <li data-id="${id}" class="movie">
-              <img class="poster" src="${poster_path}" />
+              <img class="poster" src="${backdrop_path}" />
               <h2 class="movie-title">${title}</h2>
               <div class="inner"></div>
               </li>
             `);
-    }, "");
-    console.log(films);
-    // .insertAdjacentHTML("beforeend", films);
-    document.querySelector("#movies-list").innerHTML = films;
+      }, "");
+      // .insertAdjacentHTML("beforeend", films);
+      document.querySelector("#movies-list").innerHTML = films;
+    });
+  };
+
+  const handleChange = e => {
+    app.query = e.target.value;
+    if (!app.query) {
+      getTrendingMovies(1);
+      return;
+    }
+    getMoviesByQuery(app.query);
+  };
+  refs.input.addEventListener("keyup", e => {
+    handleChange(e);
+  });
+
+  refs.moviesList.addEventListener("click", e => {
+    if (e.target.closest(".movie")) {
+      let filmId = e.target.closest(".movie").dataset.id;
+      API.fetchMoviesById(filmId).then(data => {
+        initMoviePage(data);
+      });
+    }
   });
 };
 
-const handleChange = e => {
-  const query = e.target.value;
-  getMoviesByQuery(query);
-};
+export default initMainPage;
