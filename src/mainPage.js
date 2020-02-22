1f771a694d5fca940039a721b0141384498c6864
Tatiana <eyesdwn@gmail.com>
2020-02-22 15:39:54 +0200

    add main page

--- src/mainPage.js
+++ src/mainPage.js
@@ -1,25 +1,72 @@
 import * as API from "./services/movies-api";
-import Layout from "./Layout";
-import mainPageList from "./templates/mainPageList.hbs";
-
+// import Layout from "./Layout";
+const app = {
+  page: 1
+};
 const refs = {
-  moviesList: document.querySelector(".movies-list"),
+  moviesList: document.querySelector("#movies-list"),
   buttonNext: document.querySelector(".next"),
   buttonPrev: document.querySelector(".prev"),
   paginationBoxInfo: document.querySelector(".pagination-box-info")
 };
 
-const getTrendingMovies = (page = 1) => {
+const getTrendingMovies = (page = app.page) => {
   page === 1 ? refs.buttonPrev.classList.add("hiddenButton") : null;
   API.fetchTrendingMovies(page)
     .then(data => {
-      //   createMarkup(data);
-      console.log(data);
-      Layout.render(mainPageList, "#movies-list", data);
-      //   .replace(/ /g,'')
-      //document.querySelectorAll(".article-image")
+      const films = data.reduce((acc, item) => {
+        const { id, poster_path, title } = item;
+        return (acc += `
+          <li data-id="${id}" class="movie">
+            <img class="poster" src="${poster_path}" />
+            <h2 class="movie-title">${title}</h2>
+            <div class="inner"></div>
+            </li>
+          `);
+      }, "");
+      console.log(films);
+      // .insertAdjacentHTML("beforeend", films);
+      document.querySelector("#movies-list").innerHTML = films;
     })
     .catch(err => console.log(err));
   refs.paginationBoxInfo.textContent = page;
 };
 getTrendingMovies();
+
+const handleNext = () => {
+  app.page = app.page + 1;
+  getTrendingMovies(app.page);
+  refs.buttonPrev.classList.remove("hiddenButton");
+};
+const handlePrev = () => {
+  app.page = app.page - 1;
+  getTrendingMovies(app.page);
+  refs.buttonPrev.classList.remove("hiddenButton");
+  app.page === 1 ? refs.buttonPrev.classList.add("hiddenButton") : null;
+};
+
+refs.buttonNext.addEventListener("click", handleNext);
+refs.buttonPrev.addEventListener("click", handlePrev);
+
+const getMoviesByQuery = query => {
+  API.fetchMovieByQuery(query).then(data => {
+    const films = data.reduce((acc, item) => {
+      const { id, poster_path, title } = item;
+      return (acc += `
+            <li data-id="${id}" class="movie">
+              <img class="poster" src="${poster_path}" />
+              <h2 class="movie-title">${title}</h2>
+              <div class="inner"></div>
+              </li>
+            `);
+    }, "");
+    console.log(films);
+    // .insertAdjacentHTML("beforeend", films);
+    document.querySelector("#movies-list").innerHTML = films;
+  });
+};
+
+const handleChange = e => {
+  const query = e.target.value;
+  getMoviesByQuery(query);
+};
--- src/services/movies-api.js
+++ src/services/movies-api.js
@@ -7,9 +7,6 @@ const key = "684a168c8e7f797395d8a1d5291bcabb";
 export const fetchTrendingMovies = (page = 1) => {
   const url = `/3/discover/movie?sort_by=popularity.desc&api_key=${key}&perPage=2language=en-US&page=${page}&&include_adult=false&total_results=12&total_pages=10`;
   return axios.get(url).then(response => {
-    console.log("====================================");
-    console.log(response.data.results);
-    console.log("====================================");
     response.data.results.forEach(item => {
       item.poster_path = "https://image.tmdb.org/t/p/w500" + item.poster_path;
     });
